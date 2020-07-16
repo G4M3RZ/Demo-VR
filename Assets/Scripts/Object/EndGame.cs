0f83c3ff99a6c27798d384fbcd877325eb1b69df
Pablo <pablo.0698@hotmail.com>
2020-07-16 15:00:52 -0500

    ultimos cambios visuales

--- Assets/Scripts/Object/EndGame.cs
+++ Assets/Scripts/Object/EndGame.cs
@@ -36,7 +36,7 @@ public class EndGame : MonoBehaviour
         _player.enabled = false;
         Instantiate(_uiWin, GameObject.FindGameObjectWithTag("Canvas").transform);
 
-        yield return new WaitForSeconds(2f);
+        yield return new WaitForSeconds(7f);
         
         Instantiate(_fade, _cam.transform);
         Destroy(this);
